c074fe0e9ed5a5e3b9330a92cea47e6899f4571f
nathanael <me@nattho.com>
2026-06-26 10:28:42 +0100

    add checks

--- ovi.sh
+++ ovi.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --is-ovi-install)
+      echo "True"
+      exit 0
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
 echo "Welcome to ovi"
